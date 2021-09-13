cd447b1d23badff6ef454d92eeaf05fafa89540b
tiendat159 <tangtiendat911@gmail.com>
2021-09-13 21:45:43 +0700

    commit jpql-branch

--- src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -2,6 +2,17 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+    @Query("select s from Student s")
+    List<Student> getAll();
+
+    @Query("select s from Student s where s.name = ?1")
+    List<Student> getListByName(String name);
+
+    @Query("select s from Student s where s.email = ?1")
+    List<Student> getListByEmail(String email);
 }
--- src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.springdatajpa;
 
+import com.example.springdatajpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+    private StudentRepository studentRepository;
+
     @Test
-    void contextLoads() {
+    void getAll() {
+        System.out.println(studentRepository.getAll());
     }
 
+    @Test
+    void findByName() {
+        System.out.println(studentRepository.getListByName("Dat"));
+    }
+
+    @Test
+    void findByEmail() {
+        System.out.println(studentRepository.getListByEmail("Dat123@gmail.com"));
+    }
 }
