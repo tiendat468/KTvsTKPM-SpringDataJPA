ea7946aabad2345736d4ed4a71d5a2d2e93aa607
tiendat159 <tangtiendat911@gmail.com>
2021-09-13 21:39:20 +0700

    commit native-branch

--- src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -2,6 +2,17 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+    @Query(value = "select * from Student", nativeQuery = true)
+    List<Student> getAll();
+
+    @Query(value = "select * from Student where name = ?1",nativeQuery = true )
+    List<Student> getListByName(String name);
+
+    @Query(value = "select * from Student where email = ?1",nativeQuery = true )
+    List<Student> getListByEmail(String email);
 }
